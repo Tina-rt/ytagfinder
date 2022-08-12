7395b58b9eeb69deca3edfac673d0e3f49683b21
Tina-rt <rtinahubert@gmail.com>
2022-08-12 18:40:47 +0300

    change yfinder, add comments and docstring

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# ytagfinder
+# YTagFinder
+
+A simple python module to fetch all tags of an Youtube video from an url
+
+## Requirements
+- Python 3.5 or above
+
+## Dependencies
+- bs4
+
+## Usage
+```python
+    from YTagFinder import YTagFinder
+    ytag = YTagFinder()
+    tags = ytag.find_tags('https://youtube_url')
+```
--- ytagfinder.py
+++ ytagfinder.py
@@ -2,20 +2,32 @@ from bs4 import BeautifulSoup
 import requests
 class YTagFinder:
     """
-        YTageFinder is a module to fetch a 
+        YTageFinder is a module to fetch all tags from an youtube videos
+        
+        Methods
+        --------
+        find_tags(self, youtube_video_url:str, return_type='list')
+            youtube_video_url: Url of the youtube to fetch the tags
+            return_type: type of the result ('list' for list, 'string' for str)
     """
-    result = []
+    
     def __init__(self):
         pass
 
     def find_tags(self, youtube_video_url:str, return_type='list') -> list:
-        page = requests.get('https://www.youtube.com/watch?v=CK_BCMA9yoY')
-        b = BeautifulSoup(page.content, features='html.parser')
-        f = b.findAll("meta", {"name":"keywords"})
-        tags = f[0]['content']
-        list_tags = tags.split(', ')
-        return list_tags
+        
+        if 'list' == return_type or return_type == 'string':
+            
+            page = requests.get(youtube_video_url)
+            b = BeautifulSoup(page.content, features='html.parser')
+            f = b.findAll("meta", {"name":"keywords"})
+            tags = f[0]['content']
+            if return_type == 'list':
+                list_tags = tags.split(', ')
+                return list_tags
+            return tags
+        else:
+            raise YTagFinderError("return type not recognize. All avalaible return type: list (default), string")
+class YTagFinderError(Exception):
+    pass
 
-
-y = YTagFinder()
-print(y.find_tags('https://www.youtube.com/watch?v=CK_BCMA9yoY'))
